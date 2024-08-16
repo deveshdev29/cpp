55660880841cb146e369f9d6aea641c86d710dc8
Devesh <deveshdev2909@gmail.com>
2024-08-16 19:37:51 +0530

    added new code

---
+++ fibonacci.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void fib(int n ){
+
+    int t1 = 0;
+    int t2 = 1;
+
+    int nextterm;
+
+    for(int i = 0; i < n;i++){
+        cout << t1 << endl;
+        nextterm = t1 + t2;
+        t1 = t2;
+        t2 = nextterm;
+
+    }
+}
+
+int main(){
+
+    int n;
+    cout << "How many numbers?? ";
+    cin >> n;
+
+    fib(n);
+
+    return 0;
+}
