7e3ed8b46f7179314a6b550eebe4ebe5b7f8b1a8
Devesh <deveshdev2909@gmail.com>
2024-08-17 16:17:05 +0530

    added new code

---
+++ array-min-max.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int max_num = arr[0];
+    int min_num = arr[0];
+
+    for(int i = 0; i < n;i++){
+        max_num = max(max_num,arr[i]);
+        min_num = min(min_num,arr[i]);
+    }
+    cout << "Max number is " << max_num << endl;
+    cout << "Min number is " << min_num << endl;
+    return 0;
+}
