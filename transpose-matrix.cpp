211e8cd26efcb4e7205d3e03efd8c866b52d86a8
Devesh <deveshdev2909@gmail.com>
2024-08-25 19:51:55 +0530

    added new code

--- transpose-matrix.cpp
+++ transpose-matrix.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n;
     cout << "Enter size of the matrix: ";
@@ -11,31 +12,38 @@ int main(){
     int col = n;
 
     int arr[row][col];
-    for(int i = 0; i < row; i++){
-        for(int j = 0; j < col; j++){
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
             cin >> arr[i][j];
         }
     }
 
     cout << "Matrix entered is " << endl;
-    for(int i = 0; i < row; i++){
-        for(int j = 0; j < col; j++){
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
             cout << arr[i][j] << " ";
         }
         cout << endl;
     }
 
     cout << "Transpose of the matrix: " << endl;
-    for(int i = 0; i < row; i++){
-        for(int j = i; j < col; j++){
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = i; j < col; j++)
+        {
             int temp = arr[i][j];
             arr[i][j] = arr[j][i];
             arr[j][i] = temp;
-            
         }
     }
-    for(int i = 0; i < row; i++){
-        for(int j = 0; j < col; j++){
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
             cout << arr[i][j] << " ";
         }
         cout << endl;
