ad20ed54ba774838006fbdd13dfe21f2428ef039
Devesh <deveshdev2909@gmail.com>
2024-09-11 21:21:55 +0530

    added new code

---
+++ sorted-array-recursion.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool isSorted(int arr[], int n){
+
+    if(n==1){
+        return true;
+    }
+    bool restArray = isSorted(arr+1, n-1);
+    return (arr[0] < arr[1] && restArray);
+
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    if(isSorted(arr, n)){
+        cout << "Array is Sorted" << endl;
+    }else{
+        cout << "Array is not sorted" << endl;
+    }
+    return 0;
+}
